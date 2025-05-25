c6f5e3bb77348e7709caa466a98e524d2e022f24
gcruzbull <153864295+gcruzbull@users.noreply.github.com>
2025-05-25 04:35:33 +0000

    falta metodo DELETE

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = [
+    { "label": "Estudiar backend", "done": False },
+    { "label": "Asistir a box", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def todo_list():
+    json_todos = jsonify(todos)
+    return json_todos
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    todos.append(dict(request_body))
+    return jsonify(todos)
+
+@app.route('/todos/int:1', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    return 'something'
+
+# Estas dos líneas siempre deben estar al final de tu archivo app.py
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
